20ca66ada68a024b70c100a06dd689c5d930c602
louizadr <louiz@LAPTOP-LOUIZA>
2024-04-24 16:05:21 +0200

    Initial commit

---
+++ MediaTekDocuments/view/FrmAuthentification.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace MediaTekDocuments.view
+{
+    partial class FrmAuthentification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAuthentification));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbLogin = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.lblUtilisateur = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Navy;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(-2, -2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(636, 88);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(108, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Authentification";
+            // 
+            // txbLogin
+            // 
+            this.txbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbLogin.Location = new System.Drawing.Point(74, 208);
+            this.txbLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbLogin.Name = "txbLogin";
+            this.txbLogin.Size = new System.Drawing.Size(328, 40);
+            this.txbLogin.TabIndex = 1;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbPassword.Location = new System.Drawing.Point(74, 354);
+            this.txbPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(328, 40);
+            this.txbPassword.TabIndex = 2;
+            // 
+            // lblUtilisateur
+            // 
+            this.lblUtilisateur.AutoSize = true;
+            this.lblUtilisateur.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUtilisateur.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblUtilisateur.Location = new System.Drawing.Point(66, 149);
+            this.lblUtilisateur.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUtilisateur.Name = "lblUtilisateur";
+            this.lblUtilisateur.Size = new System.Drawing.Size(170, 37);
+            this.lblUtilisateur.TabIndex = 3;
+            this.lblUtilisateur.Text = "Utilisateur";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(66, 291);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mot de passe";
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.BackColor = System.Drawing.Color.Navy;
+            this.btnConnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnexion.ForeColor = System.Drawing.Color.White;
+            this.btnConnexion.Location = new System.Drawing.Point(74, 434);
+            this.btnConnexion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(330, 89);
+            this.btnConnexion.TabIndex = 5;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = false;
+            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
+            // 
+            // FrmAuthentification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(483, 595);
+            this.Controls.Add(this.btnConnexion);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.lblUtilisateur);
+            this.Controls.Add(this.txbLogin);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmAuthentification";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Authentification utilisateur";
+            this.Load += new System.EventHandler(this.FrmAuthentification_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbLogin;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Label lblUtilisateur;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnexion;
+    }
+}
---
+++ SpecFlowMediatekDocuments/Steps/SpecFlowMediatekDocumentsSteps.cs
@@ -0,0 +1,125 @@
+﻿using MediaTekDocuments.model;
+using MediaTekDocuments.view;
+using NUnit.Framework;
+using System;
+using System.Windows.Forms;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowMediatekDocuments.Steps
+{
+    /// <summary>
+    /// Classe de tests fonctionnels SpecFlow
+    /// </summary>
+    [Binding]
+    public class SpecFlowMediatekDocumentsSteps
+    {
+        private readonly FrmMediatek frmMediatek = new FrmMediatek(null);
+
+        /// <summary>
+        /// Récupère la valeur de l'id du livre saisi
+        /// </summary>
+        /// <param name="valeur"></param>
+        [Given(@"Je saisis la valeur (.*) dans txbLivresNumRecherche")]
+        public void GivenJeSaisisLaValeurDansTxbLivresNumRecherche(string valeur)
+        {
+            TextBox TxtValeur = (TextBox)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresRecherche"].Controls["txbLivresNumRecherche"];
+            frmMediatek.Visible = true;
+            TxtValeur.Text = valeur;
+        }
+        
+        /// <summary>
+        /// Récupère le nombre de lignes affichées dans la liste des livres
+        /// Selon la valeur sélectionnée dans la comboBox du Genre
+        /// </summary>
+        /// <param name="listeAttendue">Nombre de lignes attendues dans la datagrid</param>
+        [Given(@"Je sélectionne la valeur '(.*)' dans cbxLivresGenres")]
+        public void GivenJeSelectionneLaValeurDansCbxLivresGenres(string listeAttendue)
+        {
+            ComboBox cbxLivresGenres = (ComboBox)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresInfos"].Controls["cbxLivresGenres"];
+            cbxLivresGenres.SelectedItem = listeAttendue;
+        }
+
+        /// <summary>
+        /// Récupère le nombre de lignes affichées dans la liste des livres
+        /// Selon la valeur sélectionnée dans la comboBox du Public
+        /// </summary>
+        /// <param name="listeAttendue">Nombre de lignes attendues dans la datagrid</param>
+        [Given(@"Je sélectionne la valeur '(.*)' dans cbxLivresPublics")]
+        public void GivenJeSelectionneLaValeurDansCbxLivresPublics(string listeAttendue)
+        {
+            ComboBox cbxLivresPublics = (ComboBox)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresInfos"].Controls["cbxLivresPublics"];
+            cbxLivresPublics.SelectedItem = listeAttendue;
+        }
+
+        /// <summary>
+        /// Récupère le nombre de lignes affichées dans la liste des livres
+        /// Selon la valeur sélectionnée dans la comboBox du Rayon
+        /// </summary>
+        /// <param name="listeAttendue">Nombre de lignes attendues dans la datagrid</param>
+        [Given(@"Je sélectionne la valeur '(.*)' dans cbxLivresRayons")]
+        public void GivenJeSelectionneLaValeurDansCbxLivresRayons(string listeAttendue)
+        {
+            ComboBox cbxLivresRayons = (ComboBox)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresInfos"].Controls["cbxLivresRayons"];
+            cbxLivresRayons.SelectedItem = listeAttendue;
+        }
+
+        /// <summary>
+        /// Récupère le titre du livre saisi
+        /// </summary>
+        /// <param name="titreAttendu">Récupère le titre attendu</param>
+        [Given(@"Je saisis la valeur '(.*)' dans txbLivresTitreRecherche")]
+        public void GivenJeSaisisLaValeurDansTxbLivresTitreRecherche(string titreAttendu)
+        {
+            TextBox txbLivresTitre = (TextBox)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresInfos"].Controls["txbLivresTitre"];
+            string titreObtenu = txbLivresTitre.Text;
+            Assert.AreEqual(titreAttendu, titreObtenu);
+        }
+        
+        /// <summary>
+        /// Evènement du clic sur le bouton de recherche
+        /// </summary>
+        [When(@"Je clic sur le bouton Rechercher")]
+        public void WhenJeClicSurLeBoutonRechercher()
+        {
+            Button btnLivresNumRecherche = (Button)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresRecherche"].Controls["btnLivresNumRecherche"];
+            frmMediatek.Visible = true;
+            btnLivresNumRecherche.PerformClick();
+        }
+        
+        /// <summary>
+        /// Affichage du titre dans les informations détaillées 
+        /// </summary>
+        /// <param name="titreAttendu">Récupère le titre attendu</param>
+        [Then(@"Les informations détaillées affichent le titre '(.*)'")]
+        public void ThenLesInformationsDetailleesAffichentLeTitre(string titreAttendu)
+        {
+            TextBox txbLivresTitre = (TextBox)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresInfos"].Controls["txbLivresTitre"];
+            string titreObtenu = txbLivresTitre.Text;
+            Assert.AreEqual(titreAttendu, titreObtenu);
+        }
+        
+        /// <summary>
+        /// Récupère le nombre de lignes attendues 
+        /// </summary>
+        /// <param name="listeAttendue">Nombre de lignes attendues dans la datagrid</param>
+        [Then(@"Le résultat est (.*) livres dans dgvLivresListe")]
+        public void ThenLeResultatEstLivresDansDgvLivresListe(int listeAttendue)
+        {
+            DataGridView dgvLivresListe = (DataGridView)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresRecherche"].Controls["cbxLivresRecherchesGenres"];
+            int listeObtenue = dgvLivresListe.Rows.Count;
+            Assert.AreEqual(listeAttendue, listeObtenue);
+        }
+
+        /// <summary>
+        /// Récupère le nombre de lignes attendues 
+        /// </summary>
+        /// <param name="listeAttendue">Nombre de lignes attendues dans la datagrid</param>
+        [Then(@"Le résultat est (.*) livre")]
+        public void ThenLeResultatEstLivre(int listeAttendue)
+        {
+            DataGridView dgvLivresListe = (DataGridView)frmMediatek.Controls["tabOngletsApplication"].Controls["tabLivres"].Controls["grpLivresRecherche"].Controls["cbxLivresRayons"];
+            int listeObtenue = dgvLivresListe.Rows.Count;
+            Assert.AreEqual(listeAttendue, listeObtenue);
+        }
+    }
+}
